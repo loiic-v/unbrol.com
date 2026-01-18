913ce25f29f37642175bb0b9e0e40dfd5c6b2037
Loïc <verstaevel.loic@gmail.com>
2026-01-18 20:39:39 +0100

    Enable always inlining stylesheets in Astro build
    Set the build.inlineStylesheets option to 'always' in astro.config.mjs to ensure stylesheets are always inlined during the build process.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://unbrol.com",
+  build: {
+    inlineStylesheets: "always",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
